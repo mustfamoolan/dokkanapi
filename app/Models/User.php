eb0cb82eaa53997e3587af07dde3d6a719975303
mustfamoolan <almoq3m@gmail.com>
2026-04-14 17:53:20 +0300

    Fix: Authorize users to access Filament panel

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,14 +2,21 @@
  
 namespace App\Models;
  
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasFactory, Notifiable, HasRoles;
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true; // في البداية سنسمح للكل، ويمكن لاحقاً تقييدها بالأدوار (Roles)
+    }
 
     /**
      * The attributes that are mass assignable.
